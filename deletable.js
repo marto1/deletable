7cd9148c4fb59eae74704959f71f87e8396d7fce
Martin Gergov <martingergov1@gmail.com>
2018-07-29 20:12:16 +0200

    basic game done

--- deletable.js
+++ deletable.js
@@ -1,3 +1,7 @@
+var NUMBER = 4125673;
+//track current state of number
+var currnum = NUMBER.toString().split('');
+
 function box(number) {
     var box = document.createElement("div");
     box.setAttribute("class", "box");
@@ -5,9 +9,25 @@ function box(number) {
     return box;
 }
 
-function handleMouseDown(e, b){
+function hidebox(e, b){
     b.setAttribute("class", "box hidden");
     setTimeout(function(){b.remove()}, 500);
+}
+
+function winning(e, b){
+    var p = Number(b.getAttribute("pos"));
+    currnum[p] = "_";
+    checknum = "";
+    for (var i = 0; i < currnum.length; i++) {
+	if (currnum[i] != "_"){
+	    checknum += currnum[i];
+	}
+    }
+    if (!isPrime(checknum)){
+	console.log("You lost!");
+    } else {
+	console.log("Noice!");
+    }
 }
 
 function isPrime(num) {
@@ -21,17 +41,19 @@ function numbers(number) {
     var numbox = document.createElement("div");
     for (var i = 0; i < snum.length; i++) {
 	var b = box(snum[i]);
+	b.setAttribute("pos", i);
 	numbox.appendChild(b);
 	b.onmousedown = function(e){
-	    handleMouseDown(e, this);
+	    hidebox(e, this);
+	    winning(e, this);
 	};
     }
     return numbox;
 }
 
 function main() {
-    var main = document.getElementById('main');
-    main.appendChild(numbers(22315));
+    var main = document.getElementById('main');    
+    main.appendChild(numbers(NUMBER));
 }
 
 main();
