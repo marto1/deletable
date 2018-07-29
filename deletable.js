fa8d265683b85873a39f2a780b305d30d624074b
Martin Gergov <martingergov1@gmail.com>
2018-07-29 12:21:43 +0200

    boxes js

--- deletable.js
+++ deletable.js
@@ -1,5 +1,25 @@
-main();
+BOX_STYLE = "border: solid 2px Red;";
+BOX_STYLE += "width:50px";
+
+function box(number) {
+    var box = document.createElement("div");
+    box.setAttribute("style", BOX_STYLE);
+    box.innerHTML = "<h3>" + number + "</h3>";
+    return box;
+}
+
+function numbers(number) {
+    var snum = number.toString();
+    var numbox = document.createElement("div");
+    for (var i = 0; i < snum.length; i++) {
+	numbox.appendChild(box(snum[i]));
+    }
+    return numbox;
+}
 
 function main() {
-  var main = document.getElementById('main');
+    var main = document.getElementById('main');
+    main.appendChild(numbers(22315));
 }
+
+main();
